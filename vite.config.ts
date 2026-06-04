912e7cd931e0944b5795cf18223e657efcd1e728
Desloratadine <2473609575@qq.com>
2026-06-04 19:49:36 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://qdrant.yuananhao.com',
         changeOrigin: true
       }
     }
